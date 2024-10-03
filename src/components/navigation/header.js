183b49996ea4303a42999a97a1a74d85366434ed
Halim <halimputra0701@gmail.com>
2024-10-03 11:17:19 +0700

    style(component): update styling header

--- src/components/navigation/header.js
+++ src/components/navigation/header.js
@@ -11,19 +11,19 @@ function Header() {
 						<div className='flex gap-x-[0.625rem]'>
 							<Image src='/svgs/icons/phone.svg' alt='phone-icon' width={25} height={20} />
 							<Link href='https://wa.me/6285608083342'>
-								<p className='text-textColor-500 hover:text-secondary-400 dark:text-gray-300 hover:dark:text-secondary-200'>( +62 ) 856 0808 3342</p>
+								<p className='text-textColor-gray-500 hover:text-secondary-400 dark:text-gray-300 hover:dark:text-secondary-200'>( +62 ) 856 0808 3342</p>
 							</Link>
 						</div>
 						<div className='flex items-center justify-center gap-x-[0.625rem]'>
 							<Image src='/svgs/icons/time.svg' alt='time-icon' width={20} height={20} />
-							<p className='text-textColor-500 dark:text-gray-300'>08 AM - 10 PM</p>
+							<p className='text-textColor-gray-500 dark:text-gray-300'>08 AM - 10 PM</p>
 						</div>
 					</div>
 
 					<div className='flex items-center justify-center gap-x-[0.625rem]'>
 						<Image src='/svgs/icons/location.svg' alt='location-icon' width={15} height={21} />
 						<Link href='https://maps.app.goo.gl/x6SNN1o82uUfXecGA'>
-							<p className='text-textColor-500 hover:text-secondary-400 dark:text-gray-300 hover:dark:text-secondary-200'> Jl. Dharmahusada Indah Barat Block AB 224 Surabaya</p>
+							<p className='text-textColor-gray-500 hover:text-secondary-400 dark:text-gray-300 hover:dark:text-secondary-200'> Jl. Dharmahusada Indah Barat Block AB 224 Surabaya</p>
 						</Link>
 					</div>
 				</div>
