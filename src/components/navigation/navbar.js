17178dedc0fa659be5fd31b4750b2f240b2b26a3
Halim <halimputra0701@gmail.com>
2024-10-03 11:17:29 +0700

    style(component): update styling navbar

--- src/components/navigation/navbar.js
+++ src/components/navigation/navbar.js
@@ -31,7 +31,7 @@ function Navbar() {
 	return (
 		<nav
 			id='navbar'
-			className={`${isSticky ? 'sticky top-0' : 'absolute top-[3.438rem]'} z-10 w-full bg-transparent transition-all duration-500 ease-in-out`}
+			className={`${isSticky ? 'sticky top-0' : 'absolute top-[3.438rem]'} z-30 w-full bg-transparent transition-all duration-500 ease-in-out`}
 		>
 			<div className='container mx-auto flex h-fit w-full items-center justify-center px-[3.125rem]'>
 				<div className={`flex h-[7rem] w-full items-center justify-center rounded border border-white/80 bg-white/80 px-[2.188rem] shadow-navbarShadow backdrop-blur-[1.25rem] transition-all duration-500 ease-in-out dark:border-gray-900/80 dark:bg-gray-900/80`}>
@@ -43,11 +43,11 @@ function Navbar() {
 								return (
 									<div
 										key={item.id}
-										className={`relative flex cursor-pointer items-center justify-center text-center hover:text-secondary-300 dark:hover:text-secondary-200 ${isActive ? 'text-secondary-500' : 'text-textColor-500 dark:text-gray-300'} group`}
+										className={`relative flex cursor-pointer items-center justify-center text-center hover:text-secondary-300 dark:hover:text-secondary-200 ${isActive ? 'text-primary-500' : 'text-textColor-gray-500 dark:text-gray-300'} group`}
 										onClick={() => changeRoute(router, item.slug)}
 									>
 										{item.name}
-										<span className={`absolute bottom-[-2px] left-0 h-[2px] w-full scale-x-0 transform transition-transform duration-300 ease-in-out group-hover:scale-x-100 ${isActive ? 'scale-x-100 bg-secondary-500' : 'bg-secondary-300 dark:bg-secondary-200'}`}></span>
+										<span className={`absolute bottom-[-2px] left-0 h-[2px] w-full scale-x-0 transform transition-transform duration-300 ease-in-out group-hover:scale-x-100 ${isActive ? 'scale-x-100 bg-primary-500' : 'bg-primary-300 dark:bg-primary-200'}`}></span>
 									</div>
 								);
 							})}
